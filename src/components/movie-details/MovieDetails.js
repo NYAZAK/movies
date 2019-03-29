0f3a8a4fdf9690e8a7b2c94c8bebb722483195a8
zakarianya_cgi <nyzakaria@icloud.com>
2019-03-29 09:27:11 +0100

    Feat ajoute du style pour movie element

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Style from  './MovieDetails.module.css';
+import Style from  './MovieDetails.module.scss';
 
 export default class MovieDetails extends Component{
   
     render(){
-        console.log({Style})
         return (
-            <div className="w-25 bg-secondary d-flex flex-column border p-4 text-light">
+            <div className="w-25 bg-light d-flex flex-column  p-4 text-dark">
             <h5>{this.props.films.title}</h5>
             <hr className="w-100"/>
             <div>
@@ -14,7 +13,6 @@ export default class MovieDetails extends Component{
             </div>
             <hr className="w-100"/>
             <span className="text-dark">{this.props.films.details}</span>
-        
             <p>{this.props.films.description}</p>
             </div>
         );
